63cb65c163c01bbd12c884abab08adc29eff5837
matthewfranglen <>
2008-07-21 10:12:02 +0000

    This makes the following changes:
    1) Moves all getters and setters to the bottom of the class as they are rarely relevant.
    2) Move the two inner class declarations so that they become method classes. They are only referenced by one method each. The choice was made to keep them as explicit classes because the anonymous class pattern would lose clarity.
    3) Update the methods that load the guard and idp metadata to reduce the number of redundant variables (such as the metadata directory and the loop counter).
    4) Implement the saving and loading of the IdP metadata during servlet start and stop.
    5) Clean up the application event code to explicitly state the calling context of each event and collect the code for the state together.
    6) Further commenting where appropriate.

--- src/main/java/org/guanxi/sp/engine/Bootstrap.java
+++ src/main/java/org/guanxi/sp/engine/Bootstrap.java
@@ -17,14 +17,16 @@
 package org.guanxi.sp.engine;
 
 import org.springframework.web.context.ServletContextAware;
-import org.springframework.web.context.support.RequestHandledEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.apache.log4j.Logger;
+import org.apache.xmlbeans.XmlException;
 import org.guanxi.common.log.Log4JLoggerConfig;
 import org.guanxi.common.log.Log4JLogger;
 import org.guanxi.common.metadata.IdPMetadataManager;
@@ -43,7 +45,12 @@ import javax.servlet.ServletContext;
 import java.security.Security;
 import java.security.Provider;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.ParseException;
 
 public class Bootstrap implements ApplicationListener, ApplicationContextAware, ServletContextAware {
@@ -69,10 +76,12 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
   private GuanxiJobConfig[] gxJobs = null;
 
   /**
-   * Initialise the interceptor
+   * Initialise the intercepter
    */
   public void init() {
     try {
+      File keyStoreFile, trustStoreFile;
+      
       loggerConfig.setClazz(Bootstrap.class);
 
       // Sort out the file paths for logging
@@ -93,7 +102,7 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
       }
 
       // If we don't have a keystore, create a self signed one now
-      File keyStoreFile = new File(config.getKeystore());
+      keyStoreFile = new File(config.getKeystore());
       if (!keyStoreFile.exists()) {
         try {
           SecUtils secUtils = SecUtils.getInstance();
@@ -109,7 +118,7 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
       }
 
       // Create a truststore if we don't have one
-      File trustStoreFile = new File(config.getTrustStore());
+      trustStoreFile = new File(config.getTrustStore());
       if (!trustStoreFile.exists()) {
         try {
           SecUtils secUtils = SecUtils.getInstance();
@@ -129,25 +138,8 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
       startJobs();
     }
     catch(GuanxiException ge) {
+      log.error("Issue during the initialization of the Bootstrap : ", ge);
     }
-  }
-
-  /**
-   * Called by Spring to give us the ApplicationContext
-   *
-   * @param applicationContext Spring ApplicationContext
-   * @throws org.springframework.beans.BeansException
-   */
-  public void setApplicationContext(ApplicationContext applicationContext) throws org.springframework.beans.BeansException {
-    this.applicationContext = applicationContext;
-  }
-
-  /**
-   * Sets the servlet context
-   * @param servletContext The servlet context
-   */
-  public void setServletContext(ServletContext servletContext) {
-    this.servletContext = servletContext;
   }
 
   /**
@@ -158,21 +150,21 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
    *
    * This is where we inject the job controllers into the application context, each one
    * under it's own key.
+   * 
+   * To understand the different events see
+   * http://static.springframework.org/spring/docs/2.5.x/reference/beans.html#context-functionality-events
    *
    * @param applicationEvent Spring application event
    */
   public void onApplicationEvent(ApplicationEvent applicationEvent) {
-    if (applicationEvent instanceof ContextRefreshedEvent) {
-      // Advertise the application as available for business
-      servletContext.setAttribute(Guanxi.CONTEXT_ATTR_ENGINE_CONFIG, config);
-
-      // Add the X509 stuff to the context
-      servletContext.setAttribute(Guanxi.CONTEXT_ATTR_X509_CHAIN, x509Chain);
-      
-      log.info("init : " + config.getId());
-    }
-
-    if (applicationEvent instanceof ContextClosedEvent) {
+    
+    // ContextClosedEvent
+    // Published when the ApplicationContext is closed, using the 
+    // close() method on the ConfigurableApplicationContext  
+    // interface. "Closed" here means that all singleton beans are 
+    // destroyed. A closed context has reached its end of life; it 
+    // cannot be refreshed or restarted.
+    if ( applicationEvent instanceof ContextClosedEvent ) {
       if (okToUnloadBCProvider) {
         Provider[] providers = Security.getProviders();
 
@@ -195,8 +187,109 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
         }
       }
     }
+    
+    // ContextRefreshedEvent  
+    // Published when the ApplicationContext is initialised or 
+    // refreshed, e.g. using the refresh() method on the 
+    // ConfigurableApplicationContext interface. "Initialised" 
+    // here means that all beans are loaded, post-processor 
+    // beans are detected and activated, singletons are 
+    // pre-instantiated, and the ApplicationContext object is 
+    // ready for use. A refresh may be triggered multiple times, 
+    // as long as the context hasn't been closed - provided that 
+    // the chosen ApplicationContext  actually supports such 
+    // "hot" refreshes (which e.g. XmlWebApplicationContext does 
+    // but GenericApplicationContext doesn't).
+    else if ( applicationEvent instanceof ContextRefreshedEvent ) {
+      // Advertise the application as available for business
+      servletContext.setAttribute(Guanxi.CONTEXT_ATTR_ENGINE_CONFIG, config);
 
-    if (applicationEvent instanceof RequestHandledEvent) {
+      // Add the X509 stuff to the context
+      servletContext.setAttribute(Guanxi.CONTEXT_ATTR_X509_CHAIN, x509Chain);
+      
+      log.info("init : " + config.getId());
+    }
+    
+    // ContextStartedEvent  
+    // Published when the ApplicationContext is started, using the 
+    // start() method on the ConfigurableApplicationContext 
+    // interface. "Started" here means that all life cycle beans will 
+    // receive an explicit start signal. This will typically be used 
+    // for restarting after an explicit stop, but may also be used 
+    // for starting components that haven't been configured for 
+    // auto start (e.g. haven't started on initialisation already).
+    else if ( applicationEvent instanceof ContextStartedEvent ) {
+      try {
+        loadMetadata();
+      }
+      catch ( Exception e ) {
+        log.error("Unable to load cached metadata", e);
+      }
+    }
+    
+    // ContextStoppedEvent  
+    // Published when the ApplicationContext is stopped, using the 
+    // stop() method on the ConfigurableApplicationContext interface. 
+    // "Stopped" here means that all life cycle beans will receive an 
+    // explicit stop signal. A stopped context may be restarted through 
+    // a start() call.
+    else if ( applicationEvent instanceof ContextStoppedEvent ) {
+      try {
+        saveMetadata();
+      }
+      catch ( Exception e ) {
+        log.error("Unable to save metadata to cache", e);
+      }
+    }
+  }
+  
+  /**
+   * This will load the cached metadata if it has been saved.
+   * This means that the webapp can restore the loaded metadata
+   * even if the metadata sources are not available on startup.
+   * 
+   * @throws IOException  If there is a problem reading the cache file.
+   * @throws XmlException If there is a problem parsing the XML in the cache file.
+   */
+  private void loadMetadata() throws IOException, XmlException {
+    File metadata_file;
+    
+    metadata_file = new File(config.getMetadataCacheFile());
+    if ( metadata_file.exists() ) {
+      InputStream in;
+      
+      in = new FileInputStream(metadata_file);
+      try {
+        IdPMetadataManager.getManager(servletContext).read(in);
+      }
+      finally {
+        in.close();
+      }
+    }
+  }
+  
+  /**
+   * This will save the metadata to a file for later loading when
+   * the application starts. This allows the loaded metadata to be
+   * restored even if the metadata sources are not available when
+   * the application starts up.
+   * 
+   * @throws IOException  If there is a problem writing to the cache file.
+   */
+  private void saveMetadata() throws IOException {
+    File metadata_file;
+    
+    metadata_file = new File(config.getMetadataCacheFile());
+    if ( metadata_file.exists() ) {
+      OutputStream out;
+      
+      out = new FileOutputStream(metadata_file);
+      try {
+        IdPMetadataManager.getManager(servletContext).write(out);
+      }
+      finally {
+        out.close();
+      }
     }
   }
 
@@ -216,32 +309,48 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
    * @param guardsMetadataDir The full path and name of the directory containing the Guard metadata files
    */
   private void loadGuardMetadata(String guardsMetadataDir) {
-    File guardsDir = new File(guardsMetadataDir);
-    File[] guardFiles = guardsDir.listFiles(new DirFileFilter());
+    /**
+     * Looks for directories during a search
+     * This has been moved inside this method because
+     * it is not referenced anywhere else.
+     */
+    class DirFileFilter implements FilenameFilter {
+      public boolean accept(File file, String name) {
+        return file.isDirectory();
+      }
+    }
+    
+    File[] guardDirectories;
+    int loaded;
+    
+    guardDirectories = new File(guardsMetadataDir).listFiles(new DirFileFilter());
 
-    int loaded = 0;
-    for (int count=0; count < guardFiles.length; count++) {
+    loaded = 0;
+    for ( File currentGuardDirectory : guardDirectories ) {
+      File currentGuardFile;
+      
+      currentGuardFile = new File(currentGuardDirectory, currentGuardDirectory.getName() + ".xml");
+      
       try {
+        EntityDescriptorDocument guardDocument;
+        EntityDescriptorType guardDescriptor;
+
         // Load up the SAML2 metadata for the Guard
-        EntityDescriptorDocument edDoc = EntityDescriptorDocument.Factory.parse(new File(guardFiles[count].getPath() +
-                                                                                         File.separator +
-                                                                                         guardFiles[count].getName() + ".xml"));
-        EntityDescriptorType entityDescriptor = edDoc.getEntityDescriptor();
+        guardDocument = EntityDescriptorDocument.Factory.parse(currentGuardFile);
+        guardDescriptor = guardDocument.getEntityDescriptor();
 
-        // Bung the Guard's SAML2 EntityDescriptor in the context under the Guard's entityID
-        servletContext.setAttribute(entityDescriptor.getEntityID(), entityDescriptor);
-
+        // Put the Guard's SAML2 EntityDescriptor in the context under the Guard's entityID
+        // TODO: This should probably be handled in a similar way to the IdP metadata
+        servletContext.setAttribute(guardDescriptor.getEntityID(), guardDescriptor);
         loaded++;
       }
-      catch(Exception e) {
+      catch ( Exception e ) {
         // If we get here then the Engine won't know anything about the Guard
-        log.error("Error while loading Guard metadata : " +
-                  guardFiles[count].getPath() + File.separator + guardFiles[count].getName() + ".xml",
-                  e);
+        log.error("Error while loading Guard metadata : " + currentGuardFile.getAbsolutePath(), e);
       }
-    } // for (int count=0; count < guardFiles.length; count++)
+    }
 
-    log.info("Loaded " + loaded + " of " + guardFiles.length + " Guard metadata objects");
+    log.info("Loaded " + loaded + " of " + guardDirectories.length + " Guard metadata objects");
   } // loadGuardMetadata
 
   /**
@@ -252,46 +361,45 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
    * @throws GuanxiException if an error occurs
    */
   private void loadIdPMetadata(String idpMetadataDir) throws GuanxiException {
-    File aaDir = new File(idpMetadataDir);
-    File[] idpFiles = aaDir.listFiles(new XMLFileFilter());
-    
-    for ( File current : idpFiles ) {
-    	try {
-        	EntityDescriptorDocument edDoc;
-        	EntityDescriptorType entityDescriptor;
-        	
-    		edDoc = EntityDescriptorDocument.Factory.parse(current);
-    		entityDescriptor = edDoc.getEntityDescriptor();
-    		
-    		IdPMetadataManager.getManager(servletContext).setMetadata(current.getCanonicalPath(), new IdPMetadataImpl(entityDescriptor));
-    	}
-    	catch ( Exception e ) {
-    		log.error("Error while loading IdP metadata objects", e);
-    		throw new GuanxiException(e);
-    	}
+    /**
+     * Looks for XML files during a search.
+     * This has been moved inside this method because it
+     * is not referenced anywhere else.
+     */
+    class XMLFileFilter implements FilenameFilter {
+      public boolean accept(File file, String name) {
+        return name.endsWith(".xml");
+      }
     }
-
+    
+    File[] idpFiles;
+    
+    idpFiles = new File(idpMetadataDir).listFiles(new XMLFileFilter());
+    
+    for ( File currentIdPFile : idpFiles ) {
+      try {
+        EntityDescriptorDocument idpDocument;
+        EntityDescriptorType idpDescriptor;
+        
+        idpDocument = EntityDescriptorDocument.Factory.parse(currentIdPFile);
+        idpDescriptor = idpDocument.getEntityDescriptor();
+        
+        IdPMetadataManager.getManager(servletContext).setMetadata(currentIdPFile.getCanonicalPath(), new IdPMetadataImpl(idpDescriptor));
+      }
+      catch ( Exception e ) {
+        log.error("Error while loading IdP metadata object : " + currentIdPFile.getAbsolutePath(), e);
+        throw new GuanxiException(e);
+      }
+    }
     log.info("Loaded " + idpFiles.length + " IdP metadata objects");
   } // loadIdPMetadata
 
   /**
-   * Looks for directories during a search
+   * This starts the jobs that are associated with this webapp.
+   * The jobs will be performed immediately if they have startImmediately
+   * set to true, otherwise they will be performed when the cron line
+   * indicates.
    */
-  class DirFileFilter implements FilenameFilter {
-    public boolean accept(File file, String name) {
-      return file.isDirectory();
-    }
-  }
-
-  /**
-   * Looks for xml files during a search
-   */
-  class XMLFileFilter implements FilenameFilter {
-    public boolean accept(File file, String name) {
-      return name.endsWith(".xml");
-    }
-  }
-
   private void startJobs() {
     try {
       // Get a new scheduler
@@ -336,16 +444,77 @@ public class Bootstrap implements ApplicationListener, ApplicationContextAware, 
       log.error("Error parsing job cronline", pe);
     }
   }
+  
+  // all of the getters and setters can be found below here
 
-  public void setLoggerConfig(Log4JLoggerConfig loggerConfig) { this.loggerConfig = loggerConfig; }
-  public Log4JLoggerConfig getLoggerConfig() { return loggerConfig; }
+  /**
+   * Called by Spring to give us the ApplicationContext
+   *
+   * @param applicationContext Spring ApplicationContext
+   * @throws org.springframework.beans.BeansException
+   */
+  public void setApplicationContext(ApplicationContext applicationContext) throws org.springframework.beans.BeansException {
+    this.applicationContext = applicationContext;
+  }
 
-  public void setLogger(Log4JLogger logger) { this.logger = logger; }
-  public Log4JLogger getLogger() { return logger; }
+  /**
+   * Sets the servlet context
+   * @param servletContext The servlet context
+   */
+  public void setServletContext(ServletContext servletContext) {
+    this.servletContext = servletContext;
+  }
 
+  /**
+   * Sets the logger configuration
+   * @param loggerConfig
+   */
+  public void setLoggerConfig(Log4JLoggerConfig loggerConfig) { 
+    this.loggerConfig = loggerConfig; 
+  }
+  /**
+   * Returns the logger configuration
+   * @return
+   */
+  public Log4JLoggerConfig getLoggerConfig() { 
+    return loggerConfig; 
+  }
 
-  public void setConfig(Config config) { this.config = config; }
-  public Config getConfig() { return config; }
+  /**
+   * Sets the logger
+   * @param logger
+   */
+  public void setLogger(Log4JLogger logger) { 
+    this.logger = logger; 
+  }
+  /**
+   * Returns the logger
+   * @return
+   */
+  public Log4JLogger getLogger() { 
+    return logger; 
+  }
 
-  public void setGxJobs(GuanxiJobConfig[] gxJobs) { this.gxJobs = gxJobs; }
+  /**
+   * Sets the bootstrap configuration
+   * @param config
+   */
+  public void setConfig(Config config) { 
+    this.config = config; 
+  }
+  /**
+   * Gets the bootstrap configuration
+   * @return
+   */
+  public Config getConfig() { 
+    return config; 
+  }
+
+  /**
+   * This sets the jobs that will be periodically performed.
+   * @param gxJobs
+   */
+  public void setGxJobs(GuanxiJobConfig[] gxJobs) { 
+    this.gxJobs = gxJobs; 
+  }
 }
