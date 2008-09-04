018792211795a0d88d80f5e9e8e879803fb13d5e
Alistair Young <alistair@codebrane.com>
2008-09-04 21:31:32 +0000

    Updated to use new metadata framework

--- src/main/java/org/guanxi/sp/engine/job/SAML2MetadataParser.java
+++ src/main/java/org/guanxi/sp/engine/job/SAML2MetadataParser.java
@@ -24,46 +24,77 @@ import org.guanxi.xal.saml_2_0.metadata.EntitiesDescriptorDocument;
 import org.guanxi.xal.saml_2_0.metadata.EntityDescriptorType;
 import org.guanxi.common.Utils;
 import org.guanxi.common.GuanxiException;
+import org.guanxi.common.entity.EntityFarm;
+import org.guanxi.common.definitions.Guanxi;
+import org.guanxi.common.entity.EntityManager;
+import org.guanxi.common.metadata.Metadata;
 import org.guanxi.common.job.SAML2MetadataParserConfig;
 import org.guanxi.common.job.GuanxiJobConfig;
-import org.guanxi.common.metadata.IdPMetadataManager;
-import org.guanxi.common.metadata.IdPMetadataImpl;
 
 public class SAML2MetadataParser implements Job {
-  public SAML2MetadataParser() {
-  }
+  /** Our logger */
+  private static final Logger logger = Logger.getLogger(SAML2MetadataParser.class.getName());
+
+  public SAML2MetadataParser() {}
 
   public void execute(JobExecutionContext context) throws JobExecutionException {
-    SAML2MetadataParserConfig config;
-    String metadataURL;
-    Logger logger;
+    // Get our custom config
+    SAML2MetadataParserConfig config = (SAML2MetadataParserConfig)context.getJobDetail().getJobDataMap().get(GuanxiJobConfig.JOB_KEY_JOB_CONFIG);
 
-    config = (SAML2MetadataParserConfig) context.getJobDetail().getJobDataMap().get(GuanxiJobConfig.JOB_KEY_JOB_CONFIG);
-    metadataURL = config.getMetadataURL();
-    logger = config.getLog();
+    EntitiesDescriptorDocument doc = null;
+    try {
+      // Load the metadata from the URL
+      doc = Utils.parseSAML2Metadata(config.getMetadataURL(), config.getWho());
+    }
+    catch(GuanxiException ge) {
+      logger.error("Error parsing metadata. Loading from cache", ge);
+      try {
+        // Load the metadata from the cache
+        doc = Utils.parseSAML2Metadata("file:///" + config.getMetadataCacheFile(), config.getWho());
+      }
+      catch(GuanxiException gex) {
+        logger.error("Could not load metadata from cache : " + config.getMetadataCacheFile(), gex);
+      }
+    }
 
-    logger.info("Loading SAML2 metadata from: " + metadataURL);
+    // Only proceed if we loaded the metadata from either the URL or the cache
+    if (doc == null) {
+      logger.error("No metadata available");
+      return;
+    }
+
+    EntityDescriptorType[] entityDescriptors = doc.getEntitiesDescriptor().getEntityDescriptorArray();
+
+    // Cache the metadata locally
+    try {
+      Utils.writeSAML2MetadataToDisk(doc, config.getMetadataCacheFile());
+    }
+    catch(GuanxiException ge) {
+      logger.error("Could not cache metadata to : " + config.getMetadataCacheFile(), ge);
+    }
+
+    EntityFarm farm = (EntityFarm)config.getServletContext().getAttribute(Guanxi.CONTEXT_ATTR_IDP_ENTITY_FARM);
+    EntityManager manager = farm.getEntityManagerForSource(config.getMetadataURL());
+    manager.removeMetadata();
+
+    //@todo setup TrustEngine and PKIX validation certs
 
     try {
-      EntitiesDescriptorDocument doc;
-      EntityDescriptorType[] entityDescriptors;
-      IdPMetadataManager manager;
+      for (EntityDescriptorType entityDescriptor : entityDescriptors) {
+        // Look for Service Providers
+        if (entityDescriptor.getIDPSSODescriptorArray().length > 0) {
+          logger.info("Loading IdP metadata for : " + entityDescriptor.getEntityID());
 
-      doc = Utils.parseSAML2Metadata(metadataURL, config.getWho());
-      entityDescriptors = doc.getEntitiesDescriptor().getEntityDescriptorArray();
+          Metadata metadataHandler = manager.createNewEntityHandler();
+          // This will include AttributeAuthorityDescriptor nodes
+          metadataHandler.setPrivateData(entityDescriptor);
 
-      manager = IdPMetadataManager.getManager(config.getServletContext());
-      manager.removeMetadata(metadataURL);
-
-      for (EntityDescriptorType currentMetadata : entityDescriptors) {
-        if (currentMetadata.getIDPSSODescriptorArray().length > 0) {
-          logger.info("Loading IdP metadata for : " + currentMetadata.getEntityID());
-          manager.addMetadata(metadataURL, new IdPMetadataImpl(currentMetadata));
+          manager.addMetadata(metadataHandler);
         }
       }
     }
-    catch (GuanxiException e) {
-      logger.error("Error parsing metadata", e);
+    catch(GuanxiException ge) {
+      logger.error("Could not get an entity handler from the metadata manager", ge);
     }
   }
 }
